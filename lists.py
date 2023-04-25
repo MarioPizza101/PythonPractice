dd622de989a260bfc9ae296e0e9c723b077cec9f
Alex <alexanderthomatis@icloud.com>
2023-04-25 13:33:14 -0700

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,8 @@
+family= ["Alex" , "Michelle" , "Amy" , "Michel"]
+print(family)
+family.append("John")
+family.append("Max")
+print(family)
+family.pop(0)
+print(family)
+print(family.index("John"))
