79578f72970216559be608cd073df4b1223fd53b
Alex <alexanderthomatis@icloud.com>
2023-04-25 13:38:29 -0700

    Create for_loop_dict.py

---
+++ for_loop_dict.py
@@ -0,0 +1,5 @@
+game_dict = {"Minecraft":"sandbox" , "Mario":"platformer" , "Zelda":"puzzle"}
+food_list = ["pizza" , "icecream" , "pasta" , "burger" , "chips"]
+
+for k,v in game_dict.items():
+    print(f"{k} is a {v} game.")
