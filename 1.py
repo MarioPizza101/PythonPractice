ee659c284003fdf89f6d89b045e154472b604358
Alex <alexanderthomatis@icloud.com>
2023-04-25 13:32:53 -0700

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,23 @@
+# Python
+
+var1 = 1
+
+var2 = True
+
+name = "Alex"
+message = f"My name is {name}"
+pizza= 743/4
+
+power= 10**2
+math= power*pizza
+var6='''
+hello
+my name
+is Alex'''
+
+
+print(message)
+print(pizza)
+print(power)
+print(var6)
+print(math)
